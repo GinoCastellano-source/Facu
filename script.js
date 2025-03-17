56044f891a3d7e0ac53a5eae2f5ac44f80d4edb7
GinoCastellano-source <ginocastellano075@gmail.com>
2025-03-17 14:07:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Gracias por tu mensaje. Nos pondremos en contacto contigo pronto.');
+    // Aquí podrías agregar código para enviar el formulario a un servidor
+});
